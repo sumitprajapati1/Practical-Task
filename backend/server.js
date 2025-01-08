b67bf1b331790e7a4079e034a6a880cce015fafe
sumit <prajapatisumit009@gmail.com>
2025-01-08 13:28:30 +0530

    frontend url added

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,10 @@ const dotenv = require("dotenv");
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin:"https://practical-task-fawn.vercel.app/",
+  methods: 'GET,POST,PUT,DELETE',
+}));
 
 
 connectDB();
